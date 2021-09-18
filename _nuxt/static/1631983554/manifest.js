feb77deeb7b313b587646bb23ba09e58ff122ad5
Emrin <Emrin@users.noreply.github.com>
2021-09-18 16:45:59 +0000

    deploy: 56c8b387f1e7b96f37f3e3b5ca3b3e552a23756e

---
+++ _nuxt/static/1631983554/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Ffr","\u002F","404","\u002Fblog","\u002Ffr\u002F404","\u002Ffr\u002Fblog","\u002Fcontact","\u002Ffr\u002Fcontact","\u002Fcv","\u002Ffr\u002Fcv","\u002Fdonate","\u002Ffr\u002Fdonate","\u002F404","\u002Fblog\u002Ffirst","\u002Ffr\u002Fblog\u002Ffirst"]})
